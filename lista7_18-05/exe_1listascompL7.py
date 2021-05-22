924f553d1b1cd2b2b270ad5b4c73b9775f485541
Caio Ferrari <caioferrari0484@gmail.com>
2021-05-22 17:44:35 -0300

    Improving

--- lista7_18-05/exe_1listascompL7.py
+++ lista7_18-05/exe_1listascompL7.py
@@ -1,21 +1,17 @@
 
 temp = list()
-lista = list()
+par = list()
+impar = list()
+lista = par,impar
 
 for i in range(0,7):
     temp.append(int(input('Insira um número: ')))
-
-par = list()
-impar = list()
-
 for i in temp:
     if i % 2 == 0:
-        par.append(i)
+        lista[0].append(i)
     else:
-        impar.append(i)
-
+        lista[1].append(i)
 par.sort()
 impar.sort()
-lista.append(par)
-lista.append(impar)
+
 print(f'Resultado: {lista}')
